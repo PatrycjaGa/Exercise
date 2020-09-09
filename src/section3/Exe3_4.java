3ed04593c3dc7d0df24240a2699322cc9bc56a06
Uzytkownik <pgacek03@gmail.com>
2020-09-09 20:41:53 +0200

    Exe 3.4

--- src/section3/Exe3_4.java
+++ src/section3/Exe3_4.java
@@ -1,4 +1,24 @@
 package section3;
 
 public class Exe3_4 {
+    public static String[] arrayWithLetters() {
+        String[] array = new String[]{"p", "r", "o", "g", "r", "a", "m", "o", "w", "a", "n", "i", "e"};
+        return array;
+    }
+
+    public static void showArray() {
+        for (int i = 0; i < arrayWithLetters().length; i++) {
+            System.out.print("[" + arrayWithLetters()[i] + "]" + ",");
+        }
+    }
+
+    public static void showArraysBackward() {
+        System.out.println();
+        for (int i = arrayWithLetters().length - 1; i >= 0; --i) {
+            System.out.print("[" + arrayWithLetters()[i] + "]" + ",");
+        }
+    }
+
+    
+
 }
--- src/section3/Main.java
+++ src/section3/Main.java
@@ -2,6 +2,8 @@ package section3;
 
 public class Main {
     public static void main(String[] args) {
+        Exe3_4.showArray();
+        Exe3_4.showArraysBackward();
 
     }
 }
